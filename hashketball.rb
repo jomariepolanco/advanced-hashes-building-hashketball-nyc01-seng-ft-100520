8ae845549332517729ccf49e99cb44aeadd3ed6f
Jomarie Polanco <jomariepolanco@gmail.com>
2020-09-20 13:27:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,11 @@
 # Write your code here!
+require 'pry'
+
 def game_hash
   #:home & :away => :team_name, :colors, :players = [{player stats (player name, number, shoe, points, rebounds, assists, steals, blocks, slam dunks=> stat value}]
   hash = {}
   hash[:home] = {}
   hash[:away] = {}
+  binding.pry 
   hash
 end
