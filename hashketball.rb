224b556dca067f13d091eb449a674f4b29dedfc5
Jomarie Polanco <jomariepolanco@gmail.com>
2020-09-20 13:51:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,18 +4,16 @@ require 'pry'
 def game_hash
   #:home & :away => :team_name, :colors, :players = [{player stats (player name, number, shoe, points, rebounds, assists, steals, blocks, slam dunks=> stat value}]
   final_hash = {}
-  top_level_hash = {}
   player_stats_hash = {}
-  final_hash[:home] = top_level_hash
-  final_hash[:away] = top_level_hash
-    top_level_hash[:team_name] = ""
-    top_level_hash[:colors] = ""
-    top_level_hash[:players] = []
-  final_hash[:home][:team_name] = "Brooklyn Nets"
-  final_hash[:home][:colors] = "Black, White"
-  
-  final_hash[:away][:team_name] = "Charlotte Hornets"
-  final_hash[:away][:colors] = "Turquoise, Purple"
+  final_hash[:home] = {}
+  final_hash[:away] = {}
+    final_hash[:home][:team_name] = "Brooklyn Nets"
+    final_hash[:home][:colors] = "Black, White"
+    final_hash[:home][:players] = []
+    
+    final_hash[:away][:team_name] = "Charlotte Hornets"
+    final_hash[:away][:colors] = "Turquoise, Purple"
+    final_hash[:away][:players] = []
   
  binding.pry 
   final_hash
