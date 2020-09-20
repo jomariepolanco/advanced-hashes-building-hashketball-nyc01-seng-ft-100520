3e999b9307570e491578c231f25dcf9deaa0141e
Jomarie Polanco <jomariepolanco@gmail.com>
2020-09-20 14:06:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,9 +5,8 @@ def game_hash
   #:home & :away => :team_name, :colors, :players = [{player stats (player name, number, shoe, points, rebounds, assists, steals, blocks, slam dunks=> stat value}]
   final_hash = {}
   player_stats_hash = {}
-  }
-  final_hash[:home] = []
-  final_hash[:away] = []
+  final_hash[:home] = {}
+  final_hash[:away] = {}
     final_hash[:home][:team_name] = "Brooklyn Nets"
     final_hash[:home][:colors] = "Black, White"
     final_hash[:home][:players] = []
