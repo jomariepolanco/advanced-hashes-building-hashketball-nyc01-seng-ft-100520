9bd0cc9454db165b517adc18c06aa43a0c409518
Jomarie Polanco <jomariepolanco@gmail.com>
2020-09-20 13:29:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,9 +3,11 @@ require 'pry'
 
 def game_hash
   #:home & :away => :team_name, :colors, :players = [{player stats (player name, number, shoe, points, rebounds, assists, steals, blocks, slam dunks=> stat value}]
-  hash = {}
-  hash[:home] = {}
-  hash[:away] = {}
+  final_hash = {}
+  inside_hash = {}
+  array_hash = {}
+  final_hash[:home] = inside_hash
+  final_hash[:away] = inside_hash 
   binding.pry 
-  hash
+  final_hash
 end
