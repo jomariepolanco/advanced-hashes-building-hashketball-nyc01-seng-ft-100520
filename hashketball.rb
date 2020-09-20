1d4c4f558fb41dad7053c63ee0504364b92e052a
Jomarie Polanco <jomariepolanco@gmail.com>
2020-09-20 13:26:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,9 +1,8 @@
 # Write your code here!
 def game_hash
   #:home & :away => :team_name, :colors, :players = [{player stats (player name, number, shoe, points, rebounds, assists, steals, blocks, slam dunks=> stat value}]
-  hash = {}.each do |key,value|
-    hash[:away] = {}
-    hash[:home] = {}
-  end 
+  hash = {}
+  hash[:away] = {}
+  hash[:value] = {}
   hash
 end
