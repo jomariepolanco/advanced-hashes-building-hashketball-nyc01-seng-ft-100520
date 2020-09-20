b9abf303f910b52e6a070fbb09a4e3dc304142af
Jomarie Polanco <jomariepolanco@gmail.com>
2020-09-20 14:15:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,7 +10,7 @@ def game_hash
     final_hash[:home][:team_name] = "Brooklyn Nets"
     final_hash[:home][:colors] = "Black, White"
     final_hash[:home][:players] = ["Alan Anderson", "Reggie Evans", "Brook Lopez", "Mason Plumlee", "Jason Terry"]
-    final_hash[:home][:players].each do |key|
+    final_hash[:home][:players].map do |key|
       key = player_stats_hash
     end 
     
