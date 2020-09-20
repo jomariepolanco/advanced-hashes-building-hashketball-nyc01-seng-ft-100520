327e4eedf5171994fe7b20263d7452798b7e189a
Jomarie Polanco <jomariepolanco@gmail.com>
2020-09-20 14:06:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,7 +7,7 @@ def game_hash
   player_stats_hash = {}
   }
   final_hash[:home] = []
-  final_hash[:away] = {}
+  final_hash[:away] = []
     final_hash[:home][:team_name] = "Brooklyn Nets"
     final_hash[:home][:colors] = "Black, White"
     final_hash[:home][:players] = []
