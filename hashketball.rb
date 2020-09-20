56c7f69195e15266f17b354742a714d12d4715dd
Jomarie Polanco <jomariepolanco@gmail.com>
2020-09-20 13:24:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,7 @@
 # Write your code here!
+def game_hash
+  #:home & :away => :team_name, :colors, :players = [{player stats (player name, number, shoe, points, rebounds, assists, steals, blocks, slam dunks=> stat value}]
+  hash = {}
+    
+  hash
+end
